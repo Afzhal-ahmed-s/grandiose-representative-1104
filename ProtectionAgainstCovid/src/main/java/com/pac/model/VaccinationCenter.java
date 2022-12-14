7702e59da3831fa96dba8e3564fad9af061a13ab
arijeet@8008 <arijeetchowdhury8008@gmail.com>
2022-12-14 20:36:19 +0530

    VaccinationCenter_VaccineInventory1.1

---
+++ ProtectionAgainstCovid/src/main/java/com/pac/model/VaccinationCenter.java
@@ -0,0 +1,41 @@
+package com.pac.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class VaccinationCenter {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer code;
+	@NotNull(message = "{notnull.mesage}")
+	private String centerName;
+	@NotNull(message = "{notnull.mesage}")
+	private String address;
+	@NotNull(message = "{notnull.mesage}")
+	private String city;
+	@NotNull(message = "{notnull.mesage}")
+	private String state;
+	@NotNull(message = "{notnull.mesage}")
+	private String pincode;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private Appointment appointment;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private VaccineInventory vaccineInventory;
+}
