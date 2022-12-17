491010e5d918c5bda921127d690ab249547d2feb
rajuloo7 <rajult4@gmail.com>
2022-12-17 15:48:23 +0530

    making changes to the main branch

--- ProtectionAgainstCovid/src/main/java/com/pac/dao/VaccineInventoryRepo.java
+++ ProtectionAgainstCovid/src/main/java/com/pac/dao/VaccineInventoryRepo.java
@@ -4,13 +4,14 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pac.excpetion.VaccineInventoryException;
 import com.pac.model.VaccineInventory;
-
+@Repository
 public interface VaccineInventoryRepo extends JpaRepository<VaccineInventory, Integer>{
 	
 	public List<VaccineInventory> findBydate(LocalDate date);
 	
-	public VaccineInventory  findByCode(Integer code);
+	//public VaccineInventory  findByCode(Integer code);
 }
--- ProtectionAgainstCovid/src/main/java/com/pac/serviceImplementation/AppointmentServiceImpl.java
+++ ProtectionAgainstCovid/src/main/java/com/pac/serviceImplementation/AppointmentServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.pac.dao.AdminDao;
 import com.pac.dao.AdminSessionDao;
@@ -22,7 +23,7 @@ import com.pac.model.Member;
 import com.pac.model.VaccineRegistration;
 import com.pac.service.AppointmentService;
 import com.pac.service.VaccineRegistrationService;
-
+@Service
 public class AppointmentServiceImpl implements AppointmentService{
 	
 	@Autowired
